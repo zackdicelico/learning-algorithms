5995d9eb55b473cc8dffb64f40eb131dece335dd
Zack DiCelico <zackdicelico@icloud.com>
2017-02-16 21:32:23 -0500

    add quicksort in python

---
+++ quicksort.py
@@ -0,0 +1,21 @@
+import random
+
+def quicksort(lst):
+	""" Implement the quicksort algorithm in python! """
+		
+	if len(lst) < 2:
+		return lst
+		
+	pivot = random.choice(lst)
+	
+	less = [item for item in lst if item < pivot]
+	greater = [item for item in lst if item > pivot]
+	
+	return [
+		*quicksort(less),
+		pivot,
+		*quicksort(greater)
+	]
+					
+my_list = ['3', '2', '5', '4', '1']
+quicksort(my_list)
