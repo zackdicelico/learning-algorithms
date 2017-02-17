f8462dc8850a70d120ed2794c3775abe71c819ab
Zack DiCelico <zackdicelico@icloud.com>
2017-02-16 21:37:21 -0500

    update comment

--- quicksort.py
+++ quicksort.py
@@ -1,7 +1,9 @@
 import random
 
 def quicksort(lst):
-	""" Implement the quicksort algorithm in python! """
+	""" Implement the quicksort algorithm in python! 
+	visualize at https://goo.gl/z8TDy7
+	"""
 		
 	if len(lst) < 2:
 		return lst
